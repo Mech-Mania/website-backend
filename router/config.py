d259626553e7b490226a9afc3a0ae898bae55ee9
Camleaf <bdaace09@gmail.com>
2025-08-11 20:09:07 -0400

    that didn't update on vercel

--- router/config.py
+++ router/config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 import dotenv
 import json
 
-# gather credentials and initialize firestore
+# gather credentials and initialize firestore ##
 KeyString:str = dotenv.get_key(dotenv_path='.env', key_to_get='FIREBASE_SERVICE_ACCOUNT_KEY') or ''
 cred = credentials.Certificate(json.JSONDecoder().decode(KeyString))
 firebase_admin.initialize_app(cred)

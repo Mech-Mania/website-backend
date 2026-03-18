0f517f177f50c7040105a9a0b7f297731681eddf
Camleaf <bdaace09@gmail.com>
2026-03-18 13:28:00 -0400

    add tests and score updating

--- _tests/teams.py
+++ _tests/teams.py
@@ -4,14 +4,39 @@ from dotenv import load_dotenv
 _=load_dotenv('.env')
 pw:str = os.environ['ADMIN_PASSWORD']
 
-
+# test updating team existance
 def updateTeams(teams:list[str]):
     response=requests.post(
-                    "http://127.0.0.1:8000/scoreboard/team/update",
+                    "http://127.0.0.1:8000/scoreboard/team",
                     json.dumps({"data":teams, "password":pw}), 
     )
 
     print(response.text)
 
+# test updating team scores
+def updateScores(teams:list[str],scores:list[int], game:str):
+    response=requests.post(
+                    "http://127.0.0.1:8000/scoreboard/team/score",
+                    json.dumps(
+                        {
+                            "data":[{
+                                "team":t,"score":s  
+                                } for t,s in zip(teams,scores)
+                            ],
+                            "password":pw,
+                            "game":game
+                        }
+                    )
+    )
 
-updateTeams(["atest","test2","tester3","test"])
+    print(response.text)
+
+
+
+#updateTeams(["atest","test2","tester3","test"])
+
+updateScores(
+    ["atest","test2","tester3","test"],
+    [0,0,0,0],
+    "overall"
+)
--- router/scoreboard.py
+++ router/scoreboard.py
@@ -11,9 +11,17 @@ class ArrDataReq(BaseModel):
     data:list[str]
     password:str = ""
     
+class teamWithScore(BaseModel):
+    team:str
+    score:int = 0
+
+class gameScoreUpdate(BaseModel):
+    data:list[teamWithScore] # team, score
+    password:str = ""
+    game:str = ""
 
 
-@ScoreboardRouter.post("/scoreboard/team/update")
+@ScoreboardRouter.post("/scoreboard/team")
 async def updateTeams(arrContent:ArrDataReq):
     """Updates teams so that the teams list reflects the given list"""
     
@@ -29,4 +37,23 @@ async def updateTeams(arrContent:ArrDataReq):
     
     return Response(json.dumps({"message":"Success"}))
 
-# Todo add new routes
+
+@ScoreboardRouter.post("/scoreboard/team/score")
+async def updateScores(arrContent:gameScoreUpdate):
+    """Updates the scores of all teams for an entire game"""
+    
+
+    if (not checkPassword(arrContent.password)):
+        return Response(json.dumps({"message":"Invalid Password"}))
+    
+    if (arrContent.game == "team"): # stops the program from overwriting the team column
+        return Response(json.dumps({"message":"Can not overwrite \'team\' column"}))
+
+    data:list[dict[str,str|int]] = [
+            {"team":datap.team,arrContent.game:datap.score} for datap in arrContent.data  
+    ]
+
+    # no need to be safe with updating teams since it should never happen. Leave that to implementation on website
+    _=db.table('scoreboard').upsert(data,on_conflict="team",ignore_duplicates=False).execute()
+
+    return Response(json.dumps({"message":"Success"}))
